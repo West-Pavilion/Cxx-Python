6252223fffb7a3a86f95cbbf763d4812db822ae6
West-Pavilion <liyouran17sui@outlook.com>
2024-06-11 02:06:03 +0800

    把制表符替换为四个空格

--- sr_start.cpp
+++ sr_start.cpp
@@ -2,9 +2,9 @@
 #include <filesystem>
 
 int main(){
-	std::cout << "欢迎使用 SR 协议模拟器！" << std::endl;
-	std::filesystem::current_path("py_module");
-	std::system("echo SR| main.py");
-	std::system("echo SR 协议模拟结束，请按任意键继续. . . ");
-	std::system("pause > nul");
+    std::cout << "欢迎使用 SR 协议模拟器！" << std::endl;
+    std::filesystem::current_path("py_module");
+    std::system("echo SR| main.py");
+    std::system("echo SR 协议模拟结束，请按任意键继续. . . ");
+    std::system("pause > nul");
 }
