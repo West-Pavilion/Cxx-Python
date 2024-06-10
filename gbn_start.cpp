429a3e86b6af65d216d6b13aa438b5830f3066f4
West-Pavilion <liyouran17sui@outlook.com>
2024-06-11 02:06:03 +0800

    把制表符替换为四个空格

--- gbn_start.cpp
+++ gbn_start.cpp
@@ -3,8 +3,8 @@
 
 int main() {
     std::cout << "欢迎使用 GBN 协议模拟器！" << std::endl;
-	std::filesystem::current_path("py_module");
-	std::system("echo GBN| main.py");
-	std::system("echo GBN 协议模拟结束，请按任意键继续. . . ");
-	std::system("pause > nul");
+    std::filesystem::current_path("py_module");
+    std::system("echo GBN| main.py");
+    std::system("echo GBN 协议模拟结束，请按任意键继续. . . ");
+    std::system("pause > nul");
 }
